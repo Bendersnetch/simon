37948edbbd8883e8f29adb573479cc0b6b736382
clementausserre <clement.ausserre@gmail.com>
2026-01-06 13:16:15 +0100

    suppression code inutile

--- src/ingestion-bdd/ingestion-bdd.module.ts
+++ src/ingestion-bdd/ingestion-bdd.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { IngestionBddService } from './ingestion-bdd.service';
 import { CassandraService } from 'src/cassandra/cassandra.service';
-import { KafkaConsumerService } from './kafka-consumer.service';
 
 @Module({
-  providers: [/*KafkaConsumerService, */CassandraService],
+  providers: [CassandraService],
   controllers: [IngestionBddService]
 })
 export class IngestionBddModule {}
